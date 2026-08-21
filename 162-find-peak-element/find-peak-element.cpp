bcd763ef9cb6ff68701053dcbdb7f88d877652ef
samarth927 <sg6779@srmist.edu.in>
2026-08-22 00:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (75.67%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -5,8 +5,7 @@ public:
         if(n == 1) return 0;
         if( n ==2){
             if(nums[1] > nums[0]) return 1;
-            else
-             return 0;
+            else return 0;
         }
         for(int i=1;i<n-1;i++){
             if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
