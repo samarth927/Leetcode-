599bcc0683da0668fa7807795c4e93d926501b63
samarth927 <sg6779@srmist.edu.in>
2026-08-22 23:24:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (83.29%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        string str = s;
+        
         transform(s.begin(), s.end(), s.begin(), ::tolower);      
         return s;
         
