e7c1e213dbd7d8566f20a6dd6ee114c8817cc9da
samarth927 <sg6779@srmist.edu.in>
2026-08-23 23:16:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (75.82%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -4,9 +4,18 @@ public:
         if (s.size() != t.size())
             return false;
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int count[26] = {0};
 
-        return s == t;
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0)
+                return false;
+        }
+
+        return true;
     }
 };
