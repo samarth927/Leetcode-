3d4cf316411a9ccb19e6ab8e76d5bed4e7c58dc5
samarth927 <sg6779@srmist.edu.in>
2026-09-13 23:30:23 +0530

    Time: 72 ms (38.34%) | Memory: 13.1 MB (55.83%) - LeetSync

---
+++ 864-image-overlap/image-overlap.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+        int n = img1.size();
+        int answer = 0;
+
+        for (int down = -(n - 1); down <= n - 1; down++) {
+            for (int right = -(n - 1); right <= n - 1; right++) {
+
+                int count = 0;
+
+                for (int row = 0; row < n; row++) {
+                    for (int col = 0; col < n; col++) {
+
+                        if (img1[row][col] == 1) {
+                            int newRow = row + down;
+                            int newCol = col + right;
+
+                            if (newRow >= 0 && newRow < n &&
+                                newCol >= 0 && newCol < n &&
+                                img2[newRow][newCol] == 1) {
+                                
+                                count++;
+                            }
+                        }
+                    }
+                }
+
+                answer = max(answer, count);
+            }
+        }
+
+        return answer;
+    }
+};
